1f65866ae2167e985b84756ffecebb273217ed9a
Neeraj tiwari <nt98924@gmail.com>
2023-01-10 22:32:42 +0530

    variables in javascript

---
+++ Javascript/Basic_javascript/Variables.js
@@ -0,0 +1,23 @@
+//  we can define variable in three ways.
+
+// First way 
+let a = 5;
+let b = 10;
+
+// now we have to check a or b value 
+console.log(a , b)
+
+// Second way
+var x = 8;
+var y = 2;
+
+// now we have to check x or y value 
+console.log(x , y)
+
+
+// Third way
+const p = 20;
+const q = 30;
+
+// now we have to check p or q value 
+console.log(p , q)

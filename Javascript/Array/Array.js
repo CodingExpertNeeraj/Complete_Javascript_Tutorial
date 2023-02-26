dccd5d2e49dfae47e4bc1e595362fb94e745329b
Neeraj tiwari <nt98924@gmail.com>
2023-02-26 19:26:31 +0530

    click event in javascript

--- Javascript/Array/Array.js
+++
@@ -1,8 +0,0 @@
-// how to create an array
-
-let friends = ["Rahul","Karan","Kamal","Harish"]
-
-console.log(friends)
-
-// we can check type of this  that what is it 
-console.log(typeof(friends))
---
+++ Javascript/Array_and_its_method/Array.js
@@ -0,0 +1,17 @@
+// how to create an array
+
+let friends = ["Rahul","Karan","Kamal","Harish"]
+
+console.log(friends)
+
+// we can check type of this  that what is it 
+console.log(typeof(friends))
+ 
+
+console.log(friends.length)
+// console.log(friends.push)
+// console.log(friends.pop)
+// console.log(friends.some)
+// console.log(friends.every)
+// console.log(friends.concat)
+console.log(friends.indexOf[1])

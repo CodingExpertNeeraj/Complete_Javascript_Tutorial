c25abc96a627bf530a344095371071712b7c31cd
Neeraj tiwari <nt98924@gmail.com>
2023-06-06 17:41:07 +0530

    local storage in javascript

---
+++ Javascript/ES6/Var,let,const.js
@@ -0,0 +1,39 @@
+// var and let create variables that can be reassigned another value. const creates "constant" variables that cannot be reassigned another value.
+
+
+
+// var a = 6;
+// console.log(a);
+
+// // let a = 8;
+// // console.log(a);
+
+// const a = 9;
+// console.log(a);
+
+// -----------------------------------------------
+
+// let b = 4;
+// console.log(b);
+
+// var b = 3;
+// console.log(b);
+
+// const b = 7;
+// console.log(b)   // 'b' has been already declared
+
+// --------------------------------
+
+
+// const c = 12;
+// console.log(c);
+
+// let c =19;
+// console.log(c);  // 'c' has been already declared
+
+// var c = 27;
+// console.log(c);
+
+let num = 5 + "3" - 2;
+console.log(num);
+console.log(typeof(num))
